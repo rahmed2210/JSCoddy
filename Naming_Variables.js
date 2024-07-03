8353daa2121133c528106c15954bbba02cddeb43
Rubal <rahmed2210@gmail.com>
2024-07-03 01:04:42 -0400

    Naming Variables

---
+++ Naming_Variables.js
@@ -0,0 +1,48 @@
+Naming variables
+
+Variable names should be descriptive and easy to understand. Don't be afraid to write long variable names. Now, there are some rules to keep in mind while writing names for variables. 
+
+The names of variables can only contain letters, numbers, underscores _ and dollar signs $. They also must not start with a number. For example:
+
+const number = 5;	valid
+const you&i = 'love';	invalid
+const HeUiOna4 = true;	valid
+const 4times = false	invalid
+const $time_flies = true;	valid
+Naming conventions
+You will often write variable names that contain multiple words. How should you connect the words? 
+
+The most popular way is called Camel Case. It consists of starting with lowercase letters and capitalizing the first letter of every word after the first word. Valid examples of  Camel Case are
+
+
+Variable names should be descriptive and easy to understand. Don't be afraid to write long variable names. Now, there are some rules to keep in mind while writing names for variables. 
+
+The names of variables can only contain letters, numbers, underscores _ and dollar signs $. They also must not start with a number. For example:
+
+const number = 5;	valid
+const you&i = 'love';	invalid
+const HeUiOna4 = true;	valid
+const 4times = false	invalid
+const $time_flies = true;	valid
+Naming conventions
+You will often write variable names that contain multiple words. How should you connect the words? 
+
+The most popular way is called Camel Case. It consists of starting with lowercase letters and capitalizing the first letter of every word after the first word. Valid examples of  Camel Case are
+
+userPassword
+shouldTurnOnLightSwitch
+secondList
+number. 
+Some also write variable names with underscores like 
+
+user_password
+should_turn_on_light_switch
+second_list
+number 
+In JavaScript, it is best to use the Camel Case naming convention, as most built-in commands are written that way.
+
+
+Challenge
+
+Easy
+Clean up this code and these variables. Delete the variables that don't have valid names. Also delete the variables that aren't written in Camel Case.
